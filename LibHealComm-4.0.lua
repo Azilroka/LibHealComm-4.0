f7c06a982631021af5d3ac6fdce0bea2e9d0ce62
Shadowed <shadowed.wow@gmail.com>
2009-09-26 09:29:18 -0700

    Bumping version, very good idea!

--- LibHealComm-4.0.lua
+++ LibHealComm-4.0.lua
@@ -1,5 +1,5 @@
 local major = "LibHealComm-4.0"
-local minor = 30
+local minor = 31
 assert(LibStub, string.format("%s requires LibStub.", major))
 
 local HealComm = LibStub:NewLibrary(major, minor)
